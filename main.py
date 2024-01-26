fb3168967da6f5fff748fce0e9cb8468715ce8b7
Shubham Pateria <shubham.pateria@gmail.com>
2024-01-26 16:35:21 +0800

    Clean the code for modular execution

--- main.py
+++ main.py
@@ -3,36 +3,19 @@ import tensorflow as tf
 import numpy as np
 import tqdm
 
-from agent import UvfAgent
-from train_agent import train_eval
 from LSGVP import LSGVP_Model
-from envs.twoD_mazes import env_load_fn
 from test_goalseeking import run_goalseeking
 
 ######################################## Define Args ###########################################
-max_episode_steps = 20
 replay_buffer_size = 1000 
 edge_fraction = 0.15
 edge_distance_cutoff = 1
 
-envss = ['FourRooms', 'Maze6x6', 'Maze11x11']
-env_name = envss[1] # Choose one of the environments shown above. 
-resize_factor = 5  # Inflate the environment to increase the difficulty.
+########### Load agent and environment #############
+agent = None
+eval_tf_env = None
 
-
-########################################## Train UVF Agent #######################################
-
-tf.compat.v1.reset_default_graph()
-
-tf_env = env_load_fn(env_name, max_episode_steps,
-                     resize_factor=resize_factor,
-                     terminate_on_timeout=False)
-eval_tf_env = env_load_fn(env_name, max_episode_steps,
-                          resize_factor=resize_factor,
-                          terminate_on_timeout=True)
-
-
-############################# Sample random states into replay buffer. State space is bounded, so we can assume the agent has already seen all the states during training ################
+## Sample random states into replay buffer. State space is bounded, so we can assume the agent has already seen all the states during training ##
 eval_tf_env.pyenv.envs[0].gym.set_sample_goal_args(
     prob_constraint=0.0,
     min_dist=0,
@@ -42,15 +25,15 @@ rb_vec = []
 for _ in tqdm.tnrange(replay_buffer_size):
   ts = eval_tf_env.reset()
   rb_vec.append(ts.observation['observation'].numpy()[0])
-rb_vec = np.array(rb_vec)
+rb_vec = np.array(rb_vec) #this stores randomly sampled states from which subgoals are chosen for graph building
 
 
 ########################### Generate subgoal graph model ###########################################
 lsgvp = LSGVP_Model()
 lsgvp.env = eval_tf_env
 lsgvp.actor = agent
-
 lsgvp.state_repo = copy.deepcopy(rb_vec)
+
 subgoals = lsgvp.graph_abstraction()
 lsgvp.edge_distance_cutoff = edge_distance_cutoff
 
